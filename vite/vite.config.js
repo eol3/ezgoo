5008cb357893aedc152ef615c4d7b7cf43863b93
Randy Chang <imagine90359@yahoo.com.tw>
2024-02-18 16:25:13 +0800

    load env

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,35 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-        {find: "@", replacement: path.resolve(__dirname, 'src')}
-    ],
-  },
-  plugins: [vue()],
-  server: {
-    host: 'localhost',
-  	port: '8081',
-  	proxy: {
-      '/api': {
-        target: "http://64.111.98.77:8080"
-      },
-  	}
-  },
-  build: {
-    target: 'es2020',
-    polyfillModulePreload: false
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@/assets/global.scss";
-        `
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  
+  return defineConfig({
+    resolve: {
+      alias: [
+          {find: "@", replacement: path.resolve(__dirname, 'src')}
+      ],
+    },
+    plugins: [vue()],
+    server: {
+      host: process.env.VITE_HOST,
+    	port: process.env.VITE_PORT,
+    	proxy: {
+        '/api': {
+          target: "http://64.111.98.77:8080"
+        },
+    	}
+    },
+    build: {
+      target: 'es2020',
+      polyfillModulePreload: false
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/global.scss";
+          `
+        }
       }
-    }
-  },
-})
+    },
+  })
+}
