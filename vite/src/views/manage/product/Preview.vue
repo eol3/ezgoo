d4fef54ff0df1d366884d66c52b676c075eb4710
Randy <imagine90359@yahoo.com.tw>
2025-01-15 12:17:32 +0000

    fix

--- vite/src/views/manage/product/Preview.vue
+++ vite/src/views/manage/product/Preview.vue
@@ -207,7 +207,8 @@ function getProduct() {
     queryObj.storeId = response.data.storeId
     product.value = response.data
     getStore()
-    if (product.value.barcode !== '') barcode.value = [product.value.barcode]
+    barcode.value = []
+    if (product.value.barcode !== '') barcode.value.push(product.value.barcode)
     proudctVariant.value = response.data.variant
     proudctVariant.value.forEach(element => {
       if (element.barcode !== '') barcode.value.push(element.barcode)
